b810009a02dfd53f09f85c5c285b1dd9bc142169
Eric Omine <eric.omine@iteris.com.br>
2022-05-11 09:33:44 -0300

    test: make app_test pass

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
-// import 'package:find_me_app/app/app.dart';
-// import 'package:find_me_app/counter/counter.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:find_me_app/features/app/app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// void main() {
-//   group('App', () {
-//     testWidgets('renders CounterPage', (tester) async {
-//       await tester.pumpWidget(const App());
-//       expect(find.byType(CounterPage), findsOneWidget);
-//     });
-//   });
-// }
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(Scaffold), findsOneWidget);
+    });
+  });
+}
